62af073be2b70c699d9262143b225ac6782989e7
Alon <Alon@Alon-laptop.home>
2015-08-03 19:50:16 +0300

    Fixed all

--- SummerProject2015/src/org/usfirst/frc/team2212/robot/ButtonMap.java
+++ SummerProject2015/src/org/usfirst/frc/team2212/robot/ButtonMap.java
@@ -10,10 +10,19 @@ public class ButtonMap {
 			JoystickMap.navigatorStick, 4);
 	public static JoystickButton moveArmDownButton = new JoystickButton(
 			JoystickMap.navigatorStick, 5);
+	public static JoystickButton stopPickerButton = new JoystickButton(
+			JoystickMap.navigatorStick, 6);
+	public static JoystickButton pickButton = new JoystickButton(
+			JoystickMap.navigatorStick, 1);
+	public static JoystickButton spitButton = new JoystickButton(
+			JoystickMap.navigatorStick, 2);
 
 	public static void init() {
 		stopArmButton.whenPressed(CommandMap.stopArm);
 		moveArmUpButton.whenPressed(CommandMap.moveArmUp);
 		moveArmDownButton.whenPressed(CommandMap.moveArmDown);
+		stopPickerButton.whenPressed(CommandMap.stopPicker);
+		pickButton.whenPressed(CommandMap.pick);
+		spitButton.whenPressed(CommandMap.spit);
 	}
 }
--- SummerProject2015/src/org/usfirst/frc/team2212/robot/JoystickMap.java
+++ SummerProject2015/src/org/usfirst/frc/team2212/robot/JoystickMap.java
@@ -1,23 +1,11 @@
 package org.usfirst.frc.team2212.robot;
 
-import org.usfirst.frc.team2212.commands.MoveArmDown;
-import org.usfirst.frc.team2212.commands.MoveArmUp;
-
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class JoystickMap {
 
 	public static Joystick driverRightStick = new Joystick(0);
 	public static Joystick driverLeftStick = new Joystick(1);
 	public static Joystick navigatorStick = new Joystick(2);
-
-	JoystickButton armUp = new JoystickButton(navigatorStick, 4);
-	JoystickButton armDown = new JoystickButton(navigatorStick, 5);
-
-	public JoystickMap() {
-		armUp.whenPressed(new MoveArmUp());
-		armDown.whenPressed(new MoveArmDown());
-	}
 
 }
