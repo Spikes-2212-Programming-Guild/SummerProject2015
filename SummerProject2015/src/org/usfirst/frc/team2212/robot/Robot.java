dcb4995d566f4e90a854fb242337d49b1fae64d8
Alon <Alon@Alon-laptop.home>
2015-08-03 20:05:46 +0300

    Added SmartDashboard support

--- SummerProject2015/src/org/usfirst/frc/team2212/robot/Robot.java
+++ SummerProject2015/src/org/usfirst/frc/team2212/robot/Robot.java
@@ -6,6 +6,7 @@ import org.usfirst.frc.team2212.subsystems.Picker;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
@@ -18,11 +19,27 @@ public class Robot extends IterativeRobot {
 		drivetrain = new Drivetrain();
 		arm = new Arm();
 		picker = new Picker();
+		printToSmartDashboard();
 
 	}
 
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+	}
+
+	public void printToSmartDashboard() {
+		SmartDashboard.putData(CommandMap.stopArm);
+		SmartDashboard.putData(CommandMap.moveArmUp);
+		SmartDashboard.putData(CommandMap.moveArmDown);
+		SmartDashboard.putData(CommandMap.stopPicker);
+		SmartDashboard.putData(CommandMap.pick);
+		SmartDashboard.putData(CommandMap.spit);
 	}
 
 }
