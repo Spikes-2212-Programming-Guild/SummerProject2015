dc97799517627ba81a212831e56698ea61077cd0
Alon <Alon@Alon-laptop.home>
2015-08-07 18:48:57 +0300

    Added DoubleJoystick

---
+++ SummerProject2015/src/org/usfirst/frc/team2212/robot/DoubleJoystick.java
@@ -0,0 +1,77 @@
+package org.usfirst.frc.team2212.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class DoubleJoystick extends GenericHID {
+
+	private Joystick primary, secondary;
+	private Hand primaryHand;
+
+	public DoubleJoystick(Hand primaryHand, int primaryPort, int secondaryPort) {
+		this.primaryHand = primaryHand;
+		primary = new Joystick(primaryPort);
+		secondary = new Joystick(secondaryPort);
+	}
+
+	private Joystick choose(Hand hand) {
+		return hand == primaryHand ? primary : secondary;
+	}
+
+	@Override
+	public double getX(Hand hand) {
+		return choose(hand).getX();
+	}
+
+	@Override
+	public double getY(Hand hand) {
+		return choose(hand).getY();
+	}
+
+	@Override
+	public double getZ(Hand hand) {
+		return choose(hand).getZ();
+	}
+
+	@Override
+	public double getTwist() {
+		return primary.getTwist();
+	}
+
+	@Override
+	public double getThrottle() {
+		return primary.getThrottle();
+	}
+
+	@Override
+	public double getRawAxis(int which) {
+		return primary.getRawAxis(which);
+	}
+
+	@Override
+	public boolean getTrigger(Hand hand) {
+		return choose(hand).getTrigger();
+	}
+
+	@Override
+	public boolean getTop(Hand hand) {
+		return choose(hand).getTop();
+	}
+
+	@Override
+	public boolean getBumper(Hand hand) {
+		return choose(hand).getBumper();
+	}
+
+	@Override
+	public boolean getRawButton(int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getPOV(int pov) {
+		return primary.getPOV(pov);
+	}
+
+}
